262c41f457f0b7817909ef7cf92eb0a38776b5e6
unknown <anand.verma@zmediostech.com>
2025-04-16 12:12:23 +0530

    hero height changes

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -106,7 +106,7 @@ const Hero = () => {
   return (
     <div className="relative pb-10 flex-1">
       {/* Main Container */}
-      <div className="relative h-[50vh] flex items-center justify-center">
+      <div className="relative h-[400px] flex items-center justify-center">
         <div className="relative">
           <img
             src={robot}
